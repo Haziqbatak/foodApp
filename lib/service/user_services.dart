360a34171a47c3dd7fe89a33e12af1a3f5ce9b44
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-10-15 08:00:18 +0700

    get_yser

--- lib/service/user_services.dart
+++ lib/service/user_services.dart
@@ -149,4 +149,27 @@ class UserServices {
 
     return ApiReturnValue(value: value);
   }
+
+  static Future<ApiReturnValue<User>> getUser(User user,{http.Client? client}) async {
+    client ??= http.Client();
+
+    String url = '$baseURL/user';
+
+    print('URL Get User : $url');
+
+    var response = await client.get(Uri.parse(url),
+        headers: ApiServices.headersGet(token: User.token));
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Get User Failed, please try again!');
+    }
+
+    print('Response Get User : ${response.body}');
+
+    var data = jsonDecode(response.body);
+
+    User value = User.fromJson(data['data']);
+
+    return ApiReturnValue(value: value);
+  }
 }
